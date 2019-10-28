889997a506af805eedf56d1cf4fa24cc4756cf9a
Marisa Clardy <marisa@clardy.eu>
2019-10-28 11:05:28 -0500

    Fix up default config file.

--- config/contrib-calendar.php
+++ config/contrib-calendar.php
@@ -9,14 +9,14 @@ return [
     'github' => [
         'key' => env('GITHUB_TOKEN'),
         'url' => env('GITHUB_BASE_URI'),
-        'username' => env('GITHUB_USERNAME')
+        'username' => env('GITHUB_USERNAME'),
     ],
 
     'heatmap-class' => [
-        'zero' => 'bg-gray-300',
-        'low' => 'bg-purple-200',
-        'medium' => 'bg-purple-400',
-        'high' => 'bg-purple-600',
-        'very-high' => 'bg-purple-800',
+        'zero' => '',
+        'low' => '',
+        'medium' => '',
+        'high' => '',
+        'very-high' => '',
     ],
 ];
